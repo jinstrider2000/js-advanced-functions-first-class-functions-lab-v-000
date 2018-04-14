1bf44dafe2ee68836bb7c3bc57e4cff81ebb0f7b
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 05:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,26 +13,11 @@ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 const createFareMultiplier = (number) => {
   let num = number;
   return (num) => num * number;
-}
+};
 
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
-function createMultiplier(number) {
-  let num = number;
-  return (num) => num * number;
-}
-
-function createMultiplierBonus() {
-  doubler = createMultiplier(2);
-  tripler = createMultiplier(3);
-}
-
-function multiplier(value1, value2) {
-  return value1 * value2;
-}
-
-createMultiplierBonus();
-
-doublerWithBind = multiplier.bind(null,2);
-triplerWithBind = multiplier.bind(null,3);
+const selectDifferentDrivers = (drivers, selectionFunction) => {
+  return selectionFunction(drivers);
+};
